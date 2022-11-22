300bec7baaa740f5b0a212cc52be020748691cce
pandabearcoder <vlouie00@gmail.com>
2022-11-22 21:57:47 +0800

    Register models to django admin site

--- pyconph/presentations/admin.py
+++ pyconph/presentations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Speaker, Presentation
+
+
+admin.site.register(Speaker)
+admin.site.register(Presentation)
--- pyconph/sponsors/admin.py
+++ pyconph/sponsors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SponsorType, Sponsor
+
+
+admin.site.register(SponsorType)
+admin.site.register(Sponsor)
