6b982c71f14743377682742afb3cf09e181ae111
Dan Belza <danbelza.v3@gmail.com>
2022-11-24 00:30:24 +0800

    Wagtail implementation

--- pyconph/presentations/models.py
+++ pyconph/presentations/models.py
@@ -1,7 +1,41 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
+from wagtail.models import Orderable
+from wagtail.snippets.models import register_snippet
 
 
-class Speaker(models.Model):
+class SpeakerHomeRelationship(Orderable, models.Model):
+    page = ParentalKey(
+        "home.HomePage",
+        related_name="home_speaker",
+        on_delete=models.CASCADE,
+    )
+    speaker = models.ForeignKey(
+        "presentations.Speaker",
+        related_name="speaker_home",
+        on_delete=models.CASCADE,
+    )
+    panels = [FieldPanel("speaker")]
+
+
+class KeynoteSpeakerHomeRelationship(Orderable, models.Model):
+    page = ParentalKey(
+        "home.HomePage",
+        related_name="home_keynotespeaker",
+        on_delete=models.CASCADE,
+    )
+    speaker = models.ForeignKey(
+        "presentations.Speaker",
+        related_name="keynotespeaker_home",
+        on_delete=models.CASCADE,
+    )
+    panels = [FieldPanel("speaker")]
+
+
+@register_snippet
+class Speaker(Orderable, models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     middle_name = models.CharField(max_length=64, blank=True)
@@ -10,11 +44,58 @@ class Speaker(models.Model):
         blank=True,
         help_text="Short title to call the speaker, eg: CEO of X company",
     )
-    display_photo = models.ImageField()
+    display_photo = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    introduction = models.TextField(blank=True)
+    is_featured = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.full_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Schedule(Orderable, models.Model):
+    class Days(models.TextChoices):
+        DAY1 = ("day1", "1st Day")
+        DAY2 = ("day2", "2nd Day")
+
+    page = ParentalKey(
+        "home.HomePage", related_name="schedules", on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+    description = RichTextField(blank=True)
+    location = models.CharField(max_length=255)
+
+    day = models.CharField(
+        choices=Days.choices,
+        max_length=16,
+    )
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+
+    speaker = models.ForeignKey(
+        Speaker,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
+    @property
+    def time(self):
+        fmt = "%-I:%M %p"
+        start = self.time_start.strftime(fmt)
+        end = self.time_end.strftime(fmt)
+        return f"{start} - {end}"
 
 
 class Presentation(models.Model):
-
     class PresentationTypes(models.TextChoices):
         TALK = ("talk", "Talk")
         WORKSHOP = ("workshop", "Workshop")
--- pyconph/sponsors/models.py
+++ pyconph/sponsors/models.py
@@ -1,16 +1,53 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.models import Orderable
+from wagtail.snippets.models import register_snippet
 
 
+class HomePageSponsorType(Orderable, models.Model):
+    page = ParentalKey(
+        "home.HomePage", on_delete=models.CASCADE, related_name="home_sponsor_type"
+    )
+    sponsor_type = models.ForeignKey(
+        "sponsors.SponsorType",
+        on_delete=models.CASCADE,
+        related_name="sponsor_type_home",
+    )
+
+
+class HomePageSponsor(Orderable, models.Model):
+    page = ParentalKey(
+        "home.HomePage", on_delete=models.CASCADE, related_name="home_sponsor"
+    )
+    sponsor = models.ForeignKey(
+        "sponsors.Sponsor", on_delete=models.CASCADE, related_name="sponsor_home"
+    )
+
+
+@register_snippet
 class SponsorType(models.Model):
     name = models.CharField(max_length=32)
 
+    def __str__(self):
+        return self.name
 
+
+@register_snippet
 class Sponsor(models.Model):
     name = models.CharField(max_length=256)
     info = models.TextField(blank=True)
-    logo = models.ImageField()
+    logo = models.ForeignKey(
+        "wagtaildocs.Document",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
     sponsor_type = models.ForeignKey(
         SponsorType,
         on_delete=models.PROTECT,
+        related_name="sponsors",
     )
-    sponsorship_date = models.DateField(null=True)
+    sponsorship_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
