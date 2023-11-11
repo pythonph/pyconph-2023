c7067f15059e88d8ff4da71fabfae3fa2ad2c2f1
Zorex Salvo <zorexsalvo@gmail.com>
2023-11-11 14:26:26 +0800

    chore: Setup config (#3)

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,8 +1,35 @@
+import environ
+import os
+
 from .base import *
 
-DEBUG = False
 
-try:
-    from .local import *
-except ImportError:
-    pass
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+# False if not in os.environ because of casting above
+DEBUG = env("DEBUG")
+
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = env("SECRET_KEY")
+
+# Parse database connection url strings
+# like psql://user:pass@127.0.0.1:8458/db
+DATABASES = {
+    # read os.environ['DATABASE_URL'] and raises
+    # ImproperlyConfigured exception if not found
+    #
+    # The db() method is an alias for db_url().
+    "default": env.db(),
+    # read os.environ['SQLITE_URL']
+    "extra": env.db_url("SQLITE_URL", default="sqlite:////tmp/my-tmp-sqlite.db"),
+}
